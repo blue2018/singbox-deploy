a01dc4aae7791a9fa48a7de2d9aaf76810de95ed
blue2018 <13889354+blue2018@users.noreply.github.com>
2026-01-01 12:34:08 +0800

    Update install-singbox.sh

--- install-singbox.sh
+++ install-singbox.sh
@@ -712,7 +712,7 @@ display_system_status() {
     echo -e "系统版本: \033[1;33m$OS_DISPLAY\033[0m"
     echo -e "内核信息: \033[1;33m$VER_INFO\033[0m"
     echo -e "Initcwnd: \033[1;33m${CWND_VAL}${CWND_STATUS}\033[0m"
-    echo -e "优化级别:\033[1;32m${SBOX_OPTIMIZE_LEVEL:-未检测}\033[0m"
+    echo -e "优化级别: \033[1;32m${SBOX_OPTIMIZE_LEVEL:-未检测}\033[0m"
     echo -e "伪装SNI:  \033[1;33m${RAW_SNI:-未检测}\033[0m"
     echo -e "IPv4地址: \033[1;33m${RAW_IP4:-无}\033[0m"
     echo -e "IPv6地址: \033[1;33m${RAW_IP6:-无}\033[0m"
