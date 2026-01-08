b7225c6ccde58fa8bf28fd2a5146795aff93bc66
blue2018 <13889354+blue2018@users.noreply.github.com>
2026-01-08 15:24:34 +0800

    Update install-singbox.sh

--- install-singbox.sh
+++ install-singbox.sh
@@ -668,17 +668,8 @@ LimitNOFILE=1048576
 WantedBy=multi-user.target
 EOF
 
-systemctl daemon-reexec && systemctl daemon-reload && systemctl enable sing-box --now
-sleep 1
-if systemctl is-active --quiet sing-box; then
-    local pid rss
-    pid=$(systemctl show -p MainPID --value sing-box)
-    rss=$(awk '/VmRSS/{print $2$3}' /proc/$pid/status 2>/dev/null)
-    succ "sing-box 服务启动成功 (PID=$pid | RSS=$rss)"
-else
-    error "sing-box 服务启动失败，请执行: systemctl status sing-box 查看原因"
-    exit 1
-fi
+SYSTEMD_LOG_LEVEL=info systemctl daemon-reexec && SYSTEMD_LOG_LEVEL=info systemctl daemon-reload && SYSTEMD_LOG_LEVEL=info systemctl enable sing-box --now
+
 fi
 }
 
