2f625ce1ba82bfa503f7a331ed2b6339ef982620
blue2018 <13889354+blue2018@users.noreply.github.com>
2026-01-08 10:26:24 +0800

    Update install-singbox.sh

--- install-singbox.sh
+++ install-singbox.sh
@@ -126,7 +126,6 @@ get_network_info() {
     [ -z "$v6_raw" ] && v6_raw=$(curl -6sL --max-time 3 api6.ipify.org || curl -6sL --max-time 3 ifconfig.co)
 
     # 变量清洗、环境导出与连通性测试
-    # 这种写法最简洁，且白名单过滤最彻底
     export RAW_IP4=$(echo "$v4_raw" | tr -cd '0-9.') RAW_IP6=$(echo "$v6_raw" | tr -cd 'a-fA-F0-9:')
     [ -n "$RAW_IP4" ] && ping -4 -c1 -W1 1.1.1.1 >/dev/null 2>&1 && v4_ok="\033[32m✓\033[0m"
     [ -n "$RAW_IP6" ] && ping6 -c1 -W1 2606:4700:4700::1111 >/dev/null 2>&1 && v6_ok="\033[32m✓\033[0m"
