ad80c5c2796624c701cff9f2a3d97e3dbf985a96
blue2018 <13889354+blue2018@users.noreply.github.com>
2026-01-09 14:11:55 +0800

    Update install-singbox.sh

--- install-singbox.sh
+++ install-singbox.sh
@@ -460,9 +460,7 @@ SYSCTL
         fi
     fi
 
-    apply_initcwnd_optimization "false" 
-    apply_userspace_adaptive_profile
-    apply_nic_core_boost
+    apply_initcwnd_optimization "false"; apply_userspace_adaptive_profile; apply_nic_core_boost
 }
 
 # ==========================================
