68c45efe58e7748bc10757819c36de8c342e0e4b
blue2018 <13889354+blue2018@users.noreply.github.com>
2026-01-09 10:08:32 +0800

    Update install-singbox.sh

--- install-singbox.sh
+++ install-singbox.sh
@@ -283,7 +283,8 @@ generate_cert() {
 # ==========================================
 optimize_system() {
     # 1. 执行独立探测模块获取环境画像
-    local RTT_AVG=$(probe_network_rtt) mem_total=$(probe_memory_total)
+    local RTT_AVG=$(probe_network_rtt) 
+    local mem_total=$(probe_memory_total)
     local max_udp_mb=$((mem_total * 40 / 100))
     local max_udp_pages=$((max_udp_mb * 256))
     local swappiness_val=10 busy_poll_val=0 quic_extra_msg="" VAR_BACKLOG=2000
